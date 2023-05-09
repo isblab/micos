d53556941479e42458151b4e90966009bd3b1a54
Muskaan Jindal <muskaanjinda99@gmail.com>
2023-05-09 12:00:59 +0530

    checking restraints with topo

--- modeling/modeling_script.py
+++ modeling/modeling_script.py
@@ -21,6 +21,7 @@ import IMP.pmi.restraints.crosslinking
 import IMP.pmi.restraints.em
 import IMP.pmi.dof
 import IMP.atom
+import IMP.micos
 import os
 import sys
 
@@ -39,9 +40,9 @@ max_shuffle_set2 = 75
 rex_max_temp = 1.5
 
 
-xl_data = '../../../Data/inputs/xlinks/out_inter_xl.csv'
+# xl_data = '../../../Data/inputs/xlinks/out_inter_xl.csv'
 # Topology File
-topology_file = "../../../Data/inputs/topology.txt"
+topology_file = "../Data/topology.txt"
 
 
 
@@ -66,7 +67,28 @@ class MinimumPairDistanceBindingRestraint(IMP.pmi.restraints.RestraintBase):
         print("RESTRAINT: Added MPDBR on particles", len(plist1), ":", len(plist2), "at x0:kappa", str(x0), ":",
               str(kappa))
 
+
 # wrapper for membrane inclusion restraint
+class MemnbraneInclusionRestraintC(IMP.pmi.restraints.RestraintBase):
+
+    def __init__(self, model, plist, R, r, sigma, weight=1):
+        particles = plist
+        name = 'MembraneInclusionRestraint%1%'
+        super(MemnbraneInclusionRestraintC, self).__init__(model, name=name, weight=weight)
+        res_main = IMP.micos.MembraneInclusionRestraint(particles, R, r, sigma)
+        self.rs.add_restraint(res_main)
+        print("Membrane inclusion Restraint applied")
+
+# wrapper for surface localization restraint
+class SurfaceLocalizationRestraintC(IMP.pmi.restraints.RestraintBase):
+
+    def __init__(self, model, plist,r, sigma, max_limit, weight=1):
+        particles = plist
+        name = 'SurfaceLocalizationRestraint%1%'
+        super(SurfaceLocalizationRestraintC, self).__init__(model, name=name, weight=weight)
+        res_main = IMP.micos.SurfaceLocalizationRestraint(particles, r, sigma, max_limit)
+        self.rs.add_restraint(res_main)
+        print("surface localization Restraint applied")
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -99,211 +121,217 @@ molecules = t.get_components()
 # Uncomment the following lines to get test.rmf file to visualise the system representation
 
 # Uncomment this line for verbose output of the representation
-# IMP.atom.show_with_representations(root_hier)
-# # output to RMF
-# fname = 'test.rmf'
-# rh = RMF.create_rmf_file(fname)
-# IMP.rmf.add_hierarchy(rh, root_hier)
-# IMP.rmf.save_frame(rh)
+IMP.atom.show_with_representations(root_hier)
+# output to RMF
+fname = 'test.rmf'
+rh = RMF.create_rmf_file(fname)
+IMP.rmf.add_hierarchy(rh, root_hier)
+IMP.rmf.save_frame(rh)
 
 
 # adding TM regions of all proteins to TM_regions list
 # select_by_tuple_2(start,stop,molname,copynum,statenum);  use 'None' for them which will get all
-
-TM_regions = []
+output_objects = []
+TM_regions = (149,171,'MIC60',None,None)
+mem_surface = (627,751, 'MIC60', None, None)
 
 # MIC60
-mic60 = IMP.pmi.tools.select_by_tuple_2(149,171,MIC60,None,None)
-TM_regions.append(mic60)
+mic60 = IMP.pmi.tools.select_by_tuple_2(root_hier,TM_regions,10)
+mic60_19 = IMP.pmi.tools.select_by_tuple_2(root_hier, mem_surface ,10)
 
- # MIC10
+mir = IMP.micos.MembraneInclusionRestraint(mic60, 14, 7, 1)
+output_objects.append(mir)
+print("Membrane inclusion Restraint applied")
 
+slr = IMP.micos.SurfaceLocalizationRestraint(mic60_19, 7, 1, 5)
+output_objects.append(slr)
+print("surface localization restraint applied")
 
-
-#####################################################
-##################### RESTRAINTS ####################
-#####################################################
-
-# Restraints define functions that score the model based on
-# input information.
 #
-# Restraint objects are first created in the definition.
-# To be evaluated, the restraint object must be add_to_model().
+# #####################################################
+# ##################### RESTRAINTS ####################
+# #####################################################
 #
-# In some cases, sampled parameters for restraints must be added to the DOF
-# object
-
-# The output_objects list is used to collect all restraints
-# where we want to log the output in the STAT file.
-# Each restraint should be appended to this list.
-output_objects = []
-
-# -----------------------------
-# %%%%% CONNECTIVITY RESTRAINT
+# # Restraints define functions that score the model based on
+# # input information.
+# #
+# # Restraint objects are first created in the definition.
+# # To be evaluated, the restraint object must be add_to_model().
+# #
+# # In some cases, sampled parameters for restraints must be added to the DOF
+# # object
 #
-# Restrains residues/particles that are connected in sequence
-# This should be used for any system without an atomic force field (e.g. CHARMM)
-# We apply the restraint to each molecule
-
-for m in root_hier.get_children()[0].get_children():
-    cr = IMP.pmi.restraints.stereochemistry.ConnectivityRestraint(m)
-    cr.add_to_model()
-    output_objects.append(cr)
-
-print("Connectivity restraint applied")
-
-
-# -----------------------------
-# %%%%% EXCLUDED VOLUME RESTRAINT
+# # The output_objects list is used to collect all restraints
+# # where we want to log the output in the STAT file.
+# # Each restraint should be appended to this list.
+# output_objects = []
 #
-# Keeps particles from occupying the same area in space.
-# Here, we pass a list of all molecule chains to included_objects to apply this to every residue.
-# We could also have passed root_hier to obtain the same behavior.
-#
-# resolution=1000 applies this expensive restraint to the lowest resolution for each particle.
-evr = IMP.pmi.restraints.stereochemistry.ExcludedVolumeSphere(
-                                            included_objects=[root_hier],
-                                            resolution=1000)
-output_objects.append(evr)
-
-print("Excluded volume restraint applied")
-
-
-
 # # -----------------------------
-# # %%%%% MINIMUM PAIR RESTRAINT
-# mpr1 = IMP.pmi.restraints.basic.MinimumPairRestraint(tuple_selection1=(468,546,"MTA1",0),tuple_selection2=(1,425,"RBBP4",2),distmax=10.0,root_hier=root_hier,label="MTA1-RBBP4.2_mpr1")
-# mpr2 = IMP.pmi.restraints.basic.MinimumPairRestraint(tuple_selection1=(468,546,"MTA1",1),tuple_selection2=(1,425,"RBBP4",3),distmax=10.0,root_hier=root_hier,label="MTA1.1-RBBP4.3_mpr2")
+# # %%%%% CONNECTIVITY RESTRAINT
+# #
+# # Restrains residues/particles that are connected in sequence
+# # This should be used for any system without an atomic force field (e.g. CHARMM)
+# # We apply the restraint to each molecule
 #
-# output_objects.append(mpr1)
-# output_objects.append(mpr2)
-
-
-# # -------------------------------
-# MEMBRANE RESTRAINTS
-# # -------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# -------------------------
-# %%%%% CROSSLINKING RESTRAINT
+# for m in root_hier.get_children()[0].get_children():
+#     cr = IMP.pmi.restraints.stereochemistry.ConnectivityRestraint(m)
+#     cr.add_to_model()
+#     output_objects.append(cr)
 #
-# Restrains two particles via a distance restraint based on
-# an observed crosslink.
+# print("Connectivity restraint applied")
 #
-# First, create the crosslinking database from the input file
-# The "standard keys" correspond to a crosslink csv file of the form:
 #
-# Protein1,Residue1,Protein2,Residue2
-# A,18,G,24
-# A,18,G,146
-# A,50,G,146
-# A,50,G,171
-# A,50,G,189
+# # -----------------------------
+# # %%%%% EXCLUDED VOLUME RESTRAINT
+# #
+# # Keeps particles from occupying the same area in space.
+# # Here, we pass a list of all molecule chains to included_objects to apply this to every residue.
+# # We could also have passed root_hier to obtain the same behavior.
+# #
+# # resolution=1000 applies this expensive restraint to the lowest resolution for each particle.
+# evr = IMP.pmi.restraints.stereochemistry.ExcludedVolumeSphere(
+#                                             included_objects=[root_hier],
+#                                             resolution=1000)
+# output_objects.append(evr)
 #
-# This restraint allows for ambiguity in the crosslinked residues,
-# a confidence metric for each crosslink and multiple states.
-# See the PMI documentation or the MMB book chapter for a
-# full discussion of implementing crosslinking restraints.
-
-# This first step is used to translate the crosslinking data file.
-# The KeywordsConverter maps a column label from the xl data file
-# to the value that PMI understands.
-# Here, we just use the standard keys.
-# One can define custom keywords using the syntax below.
-# For example if the Protein1 column header is "prot_1"
-# xldbkc["Protein1"]="prot_1"
-
-# The CrossLinkDataBase translates and stores the crosslink information
-# from the file "xl_data" using the KeywordsConverter.
-
-xldbkc = IMP.pmi.io.crosslink.CrossLinkDataBaseKeywordsConverter()
-xldbkc.set_standard_keys()
-
-xldb = IMP.pmi.io.crosslink.CrossLinkDataBase()
-xldb.create_set_from_file(file_name=xl_data,
-                                 converter=xldbkc)
-xlr_DSSO = IMP.pmi.restraints.crosslinking.CrossLinkingMassSpectrometryRestraint(
-                root_hier=root_hier,    # Must pass the root hierarchy to the system
-                database=xldb, # The crosslink database.
-                length=25,              # The crosslinker plus side chain length
-                resolution=1,           # The resolution at which to evaluate the crosslink
-                slope=0.0001,           # This adds a linear term to the scoring function
-                label="all_xl",                        #   to bias crosslinks towards each other
-                weight=10)       # Scaling factor for the restraint score.
-
-output_objects.append(xlr_DSSO)
-
-xlr_BDP_PIR = IMP.pmi.restraints.crosslinking.CrossLinkingMassSpectrometryRestraint(
-                root_hier=root_hier,    # Must pass the root hierarchy to the system
-                database=xldb, # The crosslink database.
-                length=33,              # The crosslinker plus side chain length
-                resolution=1,           # The resolution at which to evaluate the crosslink
-                slope=0.0001,           # This adds a linear term to the scoring function
-                label="all_xl",                        #   to bias crosslinks towards each other
-                weight=10)       # Scaling factor for the restraint score.
-
-output_objects.append(xlr_BDP_PIR)
-
-print("Cross-linking restraint applied")
-
-
-#####################################################
-###################### SAMPLING #####################
-#####################################################
-# With our representation and scoring functions determined, we can now sample
-# the configurations of our model with respect to the information.
-print("The type of run is: " + str(runType))
-print("Number of sampling frames: " + str(num_frames))
-# First shuffle all particles to randomize the starting point of the
-# system. For larger systems, you may want to increase max_translation
-
-IMP.pmi.tools.shuffle_configuration(root_hier,
-                                    max_translation=50)
-                                    # excluded_rigid_bodies=fixed_set1_core)
-                                    # hierarchies_included_in_collision=fixed_set1_core)
-
-# Shuffling randomizes the bead positions. It's good to
-# allow these to optimize first to relax large connectivity
-# restraint scores.  100-500 steps is generally sufficient.
-dof.optimize_flexible_beads(1000)
-
-IMP.pmi.dof.DegreesOfFreedom.enable_all_movers(dof)
-
-# Now, add all of the other restraints to the scoring function to start sampling
-evr.add_to_model()
-xlr.add_to_model()
-
-print("Replica Exchange Maximum Temperature : " + str(rex_max_temp))
-
-# Run replica exchange Monte Carlo sampling
-rex=IMP.pmi.macros.ReplicaExchange0(mdl,
-        root_hier=root_hier,                    # pass the root hierarchy
-        crosslink_restraints=[xlr],
-        # This allows viewing the crosslinks in Chimera. Also, there is not inter-protein ADH crosslink available. Hence it is not mentioned in this list
-        monte_carlo_temperature = 1.0,
-        replica_exchange_minimum_temperature = 1.0,
-        replica_exchange_maximum_temperature = rex_max_temp,
-	    monte_carlo_sample_objects=dof.get_movers(),  # pass all objects to be moved ( almost always dof.get_movers() )
-        global_output_directory=run_output_dir,      # The output directory for this sampling run.
-        output_objects=output_objects,          # Items in output_objects write information to the stat file.
-        monte_carlo_steps=10,                   # Number of MC steps between writing frames
-        number_of_best_scoring_models=0,        # set >0 to store best PDB files (but this is slow)
-        number_of_frames=num_frames)            # Total number of frames to run / write to the RMF file.
-        #test_mode=test_mode)                    # (Ignore this) Run in test mode (don't write anything)
-
-# Ok, now we finally do the sampling!
-rex.execute_macro()
+# print("Excluded volume restraint applied")
+#
+#
+#
+# # # -----------------------------
+# # # %%%%% MINIMUM PAIR RESTRAINT
+# # mpr1 = IMP.pmi.restraints.basic.MinimumPairRestraint(tuple_selection1=(468,546,"MTA1",0),tuple_selection2=(1,425,"RBBP4",2),distmax=10.0,root_hier=root_hier,label="MTA1-RBBP4.2_mpr1")
+# # mpr2 = IMP.pmi.restraints.basic.MinimumPairRestraint(tuple_selection1=(468,546,"MTA1",1),tuple_selection2=(1,425,"RBBP4",3),distmax=10.0,root_hier=root_hier,label="MTA1.1-RBBP4.3_mpr2")
+# #
+# # output_objects.append(mpr1)
+# # output_objects.append(mpr2)
+#
+#
+# # # -------------------------------
+# # MEMBRANE RESTRAINTS
+# # # -------------------------------
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# # -------------------------
+# # %%%%% CROSSLINKING RESTRAINT
+# #
+# # Restrains two particles via a distance restraint based on
+# # an observed crosslink.
+# #
+# # First, create the crosslinking database from the input file
+# # The "standard keys" correspond to a crosslink csv file of the form:
+# #
+# # Protein1,Residue1,Protein2,Residue2
+# # A,18,G,24
+# # A,18,G,146
+# # A,50,G,146
+# # A,50,G,171
+# # A,50,G,189
+# #
+# # This restraint allows for ambiguity in the crosslinked residues,
+# # a confidence metric for each crosslink and multiple states.
+# # See the PMI documentation or the MMB book chapter for a
+# # full discussion of implementing crosslinking restraints.
+#
+# # This first step is used to translate the crosslinking data file.
+# # The KeywordsConverter maps a column label from the xl data file
+# # to the value that PMI understands.
+# # Here, we just use the standard keys.
+# # One can define custom keywords using the syntax below.
+# # For example if the Protein1 column header is "prot_1"
+# # xldbkc["Protein1"]="prot_1"
+#
+# # The CrossLinkDataBase translates and stores the crosslink information
+# # from the file "xl_data" using the KeywordsConverter.
+#
+# xldbkc = IMP.pmi.io.crosslink.CrossLinkDataBaseKeywordsConverter()
+# xldbkc.set_standard_keys()
+#
+# xldb = IMP.pmi.io.crosslink.CrossLinkDataBase()
+# xldb.create_set_from_file(file_name=xl_data,
+#                                  converter=xldbkc)
+# xlr_DSSO = IMP.pmi.restraints.crosslinking.CrossLinkingMassSpectrometryRestraint(
+#                 root_hier=root_hier,    # Must pass the root hierarchy to the system
+#                 database=xldb, # The crosslink database.
+#                 length=25,              # The crosslinker plus side chain length
+#                 resolution=1,           # The resolution at which to evaluate the crosslink
+#                 slope=0.0001,           # This adds a linear term to the scoring function
+#                 label="all_xl",                        #   to bias crosslinks towards each other
+#                 weight=10)       # Scaling factor for the restraint score.
+#
+# output_objects.append(xlr_DSSO)
+#
+# xlr_BDP_PIR = IMP.pmi.restraints.crosslinking.CrossLinkingMassSpectrometryRestraint(
+#                 root_hier=root_hier,    # Must pass the root hierarchy to the system
+#                 database=xldb, # The crosslink database.
+#                 length=33,              # The crosslinker plus side chain length
+#                 resolution=1,           # The resolution at which to evaluate the crosslink
+#                 slope=0.0001,           # This adds a linear term to the scoring function
+#                 label="all_xl",                        #   to bias crosslinks towards each other
+#                 weight=10)       # Scaling factor for the restraint score.
+#
+# output_objects.append(xlr_BDP_PIR)
+#
+# print("Cross-linking restraint applied")
+#
+#
+# #####################################################
+# ###################### SAMPLING #####################
+# #####################################################
+# # With our representation and scoring functions determined, we can now sample
+# # the configurations of our model with respect to the information.
+# print("The type of run is: " + str(runType))
+# print("Number of sampling frames: " + str(num_frames))
+# # First shuffle all particles to randomize the starting point of the
+# # system. For larger systems, you may want to increase max_translation
+#
+# IMP.pmi.tools.shuffle_configuration(root_hier,
+#                                     max_translation=50)
+#                                     # excluded_rigid_bodies=fixed_set1_core)
+#                                     # hierarchies_included_in_collision=fixed_set1_core)
+#
+# # Shuffling randomizes the bead positions. It's good to
+# # allow these to optimize first to relax large connectivity
+# # restraint scores.  100-500 steps is generally sufficient.
+# dof.optimize_flexible_beads(1000)
+#
+# IMP.pmi.dof.DegreesOfFreedom.enable_all_movers(dof)
+#
+# # Now, add all of the other restraints to the scoring function to start sampling
+# evr.add_to_model()
+# xlr.add_to_model()
+#
+# print("Replica Exchange Maximum Temperature : " + str(rex_max_temp))
+#
+# # Run replica exchange Monte Carlo sampling
+# rex=IMP.pmi.macros.ReplicaExchange0(mdl,
+#         root_hier=root_hier,                    # pass the root hierarchy
+#         crosslink_restraints=[xlr],
+#         # This allows viewing the crosslinks in Chimera. Also, there is not inter-protein ADH crosslink available. Hence it is not mentioned in this list
+#         monte_carlo_temperature = 1.0,
+#         replica_exchange_minimum_temperature = 1.0,
+#         replica_exchange_maximum_temperature = rex_max_temp,
+# 	    monte_carlo_sample_objects=dof.get_movers(),  # pass all objects to be moved ( almost always dof.get_movers() )
+#         global_output_directory=run_output_dir,      # The output directory for this sampling run.
+#         output_objects=output_objects,          # Items in output_objects write information to the stat file.
+#         monte_carlo_steps=10,                   # Number of MC steps between writing frames
+#         number_of_best_scoring_models=0,        # set >0 to store best PDB files (but this is slow)
+#         number_of_frames=num_frames)            # Total number of frames to run / write to the RMF file.
+#         #test_mode=test_mode)                    # (Ignore this) Run in test mode (don't write anything)
+#
+# # Ok, now we finally do the sampling!
+# rex.execute_macro()
 
 # Outputs are then analyzed in a separate analysis script.

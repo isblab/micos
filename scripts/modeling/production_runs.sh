17c8c9cd5882303ba07bc8b5ce875867317b6cec
muskaan9jindal <muskaanjindal99@gmail.com>
2025-06-11 15:07:41 +0530

    Update AF3 restraints and LBS2 CG5

--- scripts/modeling/production_runs.sh
+++ scripts/modeling/production_runs.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
 
-for i in `seq 10`; do
+for i in `seq 15`; do
     folder_name="$i"
-    mpirun -np 8 /home/muskaan/imp-clean/build/setup_environment.sh python /home/muskaan/projects/micos/github/scripts/modeling/mic10_mic13_rb_mic19_1full_1N_1C_independent_fixed.py "prod" "$folder_name" /home/muskaan/projects/micos/github/inputs/data 2> err_$folder_name.log &
+    mpirun -np 8 ~/imp-clean/build/setup_environment.sh python ../scripts/modeling/mic10_dimer_mic19_1full_1N_1C_independent_fixed.py "prod" "$folder_name" ../inputs/data 2> err_$folder_name.log &
 done
 
 wait
 sleep 600 ;
 
 
-for i in `seq 11 21`; do
+for i in `seq 16 30`; do
     folder_name="$i"
-    mpirun -np 8 /home/muskaan/imp-clean/build/setup_environment.sh python /home/muskaan/projects/micos/github/scripts/modeling/mic10_mic13_rb_mic19_1full_1N_1C_independent_fixed.py "prod" "$folder_name" /home/muskaan/projects/micos/github/inputs/data 2> err_$folder_name.log &
+    mpirun -np 8 ~/imp-clean/build/setup_environment.sh python ../scripts/modeling/mic10_dimer_mic19_1full_1N_1C_independent_fixed.py "prod" "$folder_name" ../inputs/data 2> err_$folder_name.log &
 done
 
 wait
 sleep 600 ;
 
-for i in `seq 22 32`; do
+for i in `seq 31 45`; do
     folder_name="$i"
-    mpirun -np 8 /home/muskaan/imp-clean/build/setup_environment.sh python /home/muskaan/projects/micos/github/scripts/modeling/mic10_mic13_rb_mic19_1full_1N_1C_independent_fixed.py "prod" "$folder_name" /home/muskaan/projects/micos/github/inputs/data 2> err_$folder_name.log &
+    mpirun -np 8 ~/imp-clean/build/setup_environment.sh python ../scripts/modeling/mic10_dimer_mic19_1full_1N_1C_independent_fixed.py "prod" "$folder_name" ../inputs/data 2> err_$folder_name.log &
 done
 
 wait
 sleep 600 ;
 
-for i in `seq 33 43`; do
+for i in `seq 46 50`; do
     folder_name="$i"
-    mpirun -np 8 /home/muskaan/imp-clean/build/setup_environment.sh python /home/muskaan/projects/micos/github/scripts/modeling/mic10_mic13_rb_mic19_1full_1N_1C_independent_fixed.py "prod" "$folder_name" /home/muskaan/projects/micos/github/inputs/data 2> err_$folder_name.log &
+    mpirun -np 8 ~/imp-clean/build/setup_environment.sh python ../scripts/modeling/mic10_dimer_mic19_1full_1N_1C_independent_fixed.py "prod" "$folder_name" ../inputs/data 2> err_$folder_name.log &
 done
-
-wait
-sleep 600 ;
-
-for i in `seq 44 50`; do
-    folder_name="$i"
-    mpirun -np 8 /home/muskaan/imp-clean/build/setup_environment.sh python /home/muskaan/projects/micos/github/scripts/modeling/mic10_mic13_rb_mic19_1full_1N_1C_independent_fixed.py "prod" "$folder_name" /home/muskaan/projects/micos/github/inputs/data 2> err_$folder_name.log &
-done
-
-exit

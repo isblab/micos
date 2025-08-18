4309269091e47ce77d794aa4ff77a2503fe70907
muskaan9jindal <muskaanjindal99@gmail.com>
2025-08-18 14:14:40 +0530

    Organizing direc and removing topo inconsistent af3 pred

--- scripts/analysis/sampcon.sh
+++ scripts/analysis/sampcon.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-$imp  python  ~/imp-clean/imp/modules/sampcon/pyext/src/exhaust.py -n micos -a -m cpu_omp -c 0 -cc 2 -pr -d model_analysis/density.txt -gp -g 2.0  -sa model_analysis/A_models_clust1.txt -sb model_analysis/B_models_clust1.txt  -ra model_analysis/A_models_clust1.rmf3 -rb model_analysis/B_models_clust1.rmf3
+$imp  python  ~/imp-clean/imp/modules/sampcon/pyext/src/exhaust.py -n micos -a -m cpu_omp -cc 2 -pr -d model_analysis/density.txt -gp -g 2.0  -sa model_analysis/A_models_clust1.txt -sb model_analysis/B_models_clust1.txt  -ra model_analysis/A_models_clust1.rmf3 -rb model_analysis/B_models_clust1.rmf3
 
 echo "Done!"

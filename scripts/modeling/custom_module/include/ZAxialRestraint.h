9755e4ff40857960fde1fc0a72376daf300f26a9
muskaan9jindal <muskaanjindal99@gmail.com>
2025-09-24 14:21:35 +0530

    Update custom module

--- scripts/modeling/custom_module/include/ZAxialRestraint.h
+++ scripts/modeling/custom_module/include/ZAxialRestraint.h
@@ -28,7 +28,7 @@ class IMPMICOSEXPORT ZAxialRestraint : public IMP::Restraint {
         IMP_OBJECT_METHODS(ZAxialRestraint);  //add the usual IMP object methods
 
     private:
-        double getDistance( double z) const;
+        double get_squared_distance( double z) const;
 
 };
 
